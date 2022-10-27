d1d623efb5bbfc7ccb7345476cb03d8afd5ab38a
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-10-27 23:51:36 +0600

    finally done every thing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,6 +29,7 @@ const Home = () => {
                     />
                     <Carousel.Caption>
                         <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import notFound from "../../assets/images/not found/download.png"
 
 const NotFound = () => {
     return (
         <div>
-            <h1>Sorry page not found</h1>
+            <h1 className='text-center'>Sorry page not found</h1>
+            <img className='w-75 ms-5 ps-5 mb-5' src={notFound} alt="" />
+            <br />
             <Link to="/">Go to home</Link>
         </div>
     );
